b9af74a4dbf79147913f7870221be961415ac608
rockyluke <rockyluke@offline.net>
2016-02-14 16:12:21 +0100

    Add firstboot.sh

---
+++ firstboot.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Copyright (c) 2015, Dalenys Group
+#
+# Permission  to use,  copy, modify,  and/or  distribute this  software for  any
+# purpose  with  or without  fee  is hereby  granted,  provided  that the  above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS"  AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+# REGARD TO  THIS SOFTWARE INCLUDING  ALL IMPLIED WARRANTIES  OF MERCHANTABILITY
+# AND FITNESS.  IN NO EVENT SHALL  THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+# INDIRECT, OR  CONSEQUENTIAL DAMAGES OR  ANY DAMAGES WHATSOEVER  RESULTING FROM
+# LOSS OF USE, DATA OR PROFITS,  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+# OTHER  TORTIOUS ACTION,  ARISING  OUT OF  OR  IN CONNECTION  WITH  THE USE  OR
+# PERFORMANCE OF THIS SOFTWARE.
+
+# Disable SMS
+sudo pmset -a sms 0
+
+# Daily software update check
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Change location screen capture
+defaults write com.apple.screencapture location ~/Pictures/
+
+# EOF
